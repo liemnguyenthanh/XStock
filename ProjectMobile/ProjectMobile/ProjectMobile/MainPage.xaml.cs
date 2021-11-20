e43467941cea183a1c59fa158eceb9df83d139e6
Kai Moore <haohao12012001@gmail.com>
2021-11-20 07:50:15 +0700

    commit 3 interfaces (chon hang, chon size, thanh toan)

---
+++ ProjectMobile/ProjectMobile/ProjectMobile/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ProjectMobile
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new ChonHang());
+        }
+
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new ChonSize());
+        }
+
+        private void Button_Clicked_2(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new ThanhToan());
+        }
+    }
+}
